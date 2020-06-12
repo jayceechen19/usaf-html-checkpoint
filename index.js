30579fc189c5a2471fd6ec3bc4e66149dc458305
bbarnhi <barnhill0126@gmail.com>
2020-06-12 11:35:09 -0400

    commit 4

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded',function(){
+    var loginButton = document.getElementById('login')
+    loginButton.addEventListener('click', function(){
+        loginButton.href='title.html'
+    })
+    
+})
+
