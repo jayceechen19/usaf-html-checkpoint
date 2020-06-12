d7ba4f45d6b242b38a8d3245ff358bca88e3e1bf
bbarnhi <barnhill0126@gmail.com>
2020-06-12 13:02:29 -0400

    FINAL

--- title.js
+++ title.js
@@ -1,6 +1,23 @@
+console.log('can you see me')
 document.addEventListener('DOMContentLoaded',function(){
+            console.log("can you see me")
     var imgButton = document.getElementById('toComments')
     imgButton.addEventListener('click', function(){
         window.location.href='comments.html';
     }) 
+
+
+    var searchButton = document.getElementById('searchButton')
+    
+    searchButton.addEventListener('click', function(){
+
+        var searchBar = document.getElementById('searchBar')
+        let value = searchBar.nodeValue.toLowerCase();
+        console.log(value)
+//        Get form element 
+//        get text from field
+//        if name from imgFolder contains userSubmittedText   displayMoviePoster
+
+        window.location.href='comments.html';    
+    })
 })
